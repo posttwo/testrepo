09df11ea5e68ac190f69309fbc2cef567719ca50
Posttwo <posttwo@users.noreply.github.com>
2018-06-28 19:43:42 +0100

    Create cog.py

---
+++ cog/cog.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class Mycog:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_member_update(self, before, after):
+        """This does stuff!"""
+
+        if len(after.nick) < 3:
+			await self.bot.change_nickname(after, "Censored")
+			
+        await self.bot.say("I can do stuff!")
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))
