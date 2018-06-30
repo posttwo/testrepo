ae665c4ce42a07322d6bb12ac7f496e4cfd41cb5
Posttwo <posttwo@users.noreply.github.com>
2018-06-30 17:24:59 +0100

    Update cog.py

--- cog/cog.py
+++ cog/cog.py
@@ -48,10 +48,17 @@ class Mycog:
                 msg = msg.format(member)
                 
                 await self.bot.say(msg)
+                self.save()
     
             else:
                 await self.bot.send_cmd_help(ctx)
     
+    
+    @commands.group(pass_context=True, no_pm=True, name='remove')
+    @checks.mod_or_permissions(manage_messages=True)
+    async def punish_remove(self, ctx, member: discord.Member):
+        self.json[ctx.message.server.id].pop(member.id, None)
+        self.save()
     
     @punish.command(pass_context=True, no_pm=True, name='list')
     async def punish_list(self, ctx):
